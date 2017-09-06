f1d536d59f30bc67d5ae42a7170bfe313e7c9ba7
Patrick Garcia <patrickvg@ufcpsa.edu.br>
2017-09-06 15:43:40 -0300

    testando, primeiro commit do faq

---
+++ js/js.js
@@ -0,0 +1,90 @@
+//Esconder a lista do menu
+function toggle(obj) {
+    var el = document.getElementById(obj);
+    if (el.style.display != 'none') {
+        el.style.display = 'none';
+    } else {
+        el.style.display = '';
+    }
+}
+
+//Janela Modal
+$(document).ready(function() {
+    $("a[rel=modal]").click(function(ev) {
+        ev.preventDefault();
+
+        var id = $(this).attr("href");
+
+        var alturaTela = $(document).height();
+        var larguraTela = $(window).width();
+
+        //colocando o fundo preto
+        $('#mascara').css({
+            'width': larguraTela,
+            'height': alturaTela
+        });
+        $('#mascara').fadeIn(1000);
+        $('#mascara').fadeTo("slow", 0.8);
+
+        var left = ($(window).width() / 2) - ($(id).width() / 2);
+        var top = ($(window).height() / 2) - ($(id).height() / 2);
+
+        $(id).css({
+            'top': top,
+            'left': left
+        });
+        $(id).show();
+    });
+
+    $("#mascara").click(function() {
+        $(this).hide();
+        $(".window").hide();
+    });
+
+    $('.fechar').click(function(ev) {
+        ev.preventDefault();
+        $("#mascara").hide();
+        $(".window").hide();
+    });
+});
+
+
+        $(document).ready(function(){
+        $("a[rel=modal]").click( function(ev){
+        ev.preventDefault();
+        
+        var id = $(this).attr("href");  
+        var alturaTela = $(document).height();
+        var larguraTela = $(window).width();
+        
+        //colocando a mascara no modal
+        $('#mascara').css({'width':larguraTela,'height':alturaTela});
+        $('#mascara').fadeIn(1000);
+        $('#mascara').fadeTo("slow",0.8);
+        
+        var left = ($(window).width() /2) - ( $(id).width() / 2 );
+        var top = ($(window).height() / 2) - ( $(id).height() / 2 );
+        
+        $(id).css({'top':top,'left':left});
+        $(id).show();
+        });
+        
+        $("#mascara").click( function(){
+        $(this).hide();
+        $(".window").hide();
+        });
+        
+        $('.fechar').click(function(ev){
+        ev.preventDefault();
+        $("#mascara").hide();
+        $(".window").hide();
+        });
+        });
+
+        // Fechando janelas modal com a tecla ESC
+        $(document).keyup(function(event) {
+                if (event.which == 27) {
+                     $(".window").hide();
+                     $("#mascara").hide();
+            }
+            });
